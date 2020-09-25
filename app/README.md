f2b7679d82c43d5bf235edbf349cc3868c378227
Relz <relz0071@gmail.com>
2020-09-25 21:30:12 +0300

    Реализовать Time Service

---
+++ app/README.md
@@ -0,0 +1,19 @@
+# Time Service
+
+A web-service displaying server time and name.
+
+## Run
+
+```
+dart bin/server.dart
+```
+
+Default port is 8080, but you can specify another port:
+
+```
+dart bin/server.dart --port=8081
+```
+
+```
+dart bin/server.dart -p 8081
+```
---
+++ app/bin/server.dart
@@ -0,0 +1,44 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:args/args.dart';
+import 'package:shelf/shelf.dart' as shelf;
+import 'package:shelf/shelf_io.dart' as io;
+
+Future<void> main(List<String> args) async {
+  final ArgParser parser = ArgParser()..addOption('port', abbr: 'p');
+  final ArgResults result = parser.parse(args);
+
+  final String portStr = result['port'] as String // ignore: avoid_as
+      ??
+      Platform.environment['PORT'] ??
+      '8080';
+  final int port = int.tryParse(portStr);
+
+  if (port == null) {
+    stdout.writeln('Could not parse port value "$portStr" into a number.');
+    // 64: command line usage error
+    exitCode = 64;
+    return;
+  }
+
+  final shelf.Handler handler = const shelf.Pipeline()
+      .addMiddleware(shelf.logRequests())
+      .addHandler(_echoRequest);
+
+  final HttpServer server = await io.serve(handler, 'localhost', port);
+  // ignore: avoid_print
+  print('Serving at http://${server.address.host}:${server.port}');
+}
+
+shelf.Response _echoRequest(shelf.Request request) => shelf.Response.ok(
+      json.encode(
+        <String, String>{
+          'hostname': Platform.localHostname,
+          'local_time': DateTime.now().toString(),
+        },
+      ),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    );
